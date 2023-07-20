452b9a2a52317072c6df826a308f1b68438106db
Geoport <destek@geoport.com.tr>
2023-07-20 21:24:17 +0300

    update

--- server_script.sh
+++ server_script.sh
@@ -5,6 +5,4 @@ sudo certbot certonly --nginx -d beta.soilprime.com
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo apt-get install docker-compose -y
-git clone https://github.com/geoport/TestNext.git
-cd TestNext
 sudo docker-compose up --build
